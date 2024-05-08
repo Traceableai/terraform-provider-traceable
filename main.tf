6aa07a8ba63221a95f4506ae35887ca7d867165b
Meghana Pedduri <meghana.pedduri@traceable.ai>
2024-05-08 13:00:46 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -4,65 +4,52 @@ terraform {
       source  = "terraform.local/local/example"
       version = "0.0.1"
     }
-    aws={
-      source = "hashicorp/aws"
-      version = "5.35.0"
-    }
+    # aws={
+    #   source = "hashicorp/aws"
+    #   version = "5.35.0"
+    # }
   }
-    backend "s3" {
-    bucket         = "traceable-provider-store"
-    key            = "traceable-provider-store"
-    region         = "us-west-2"
-  }
+  #   backend "s3" {
+  #   bucket         = "traceable-provider-store"
+  #   key            = "traceable-provider-store"
+  #   region         = "us-west-2"
+  # }
 }
 
-data "aws_secretsmanager_secret_version" "api_token" {
-  secret_id = "you secret manager arn"
-}
+# data "aws_secretsmanager_secret_version" "api_token" {
+#   secret_id = "you secret manager arn"
+# }
 
-output "api_token" {
-  value=jsondecode(data.aws_secretsmanager_secret_version.api_token.secret_string)["api_token"]
-  sensitive = true
-}
+# output "api_token" {
+#   value=jsondecode(data.aws_secretsmanager_secret_version.api_token.secret_string)["api_token"]
+#   sensitive = true
+# }
 
 provider "example" {
-  platform_url="cluster_url"
-  api_token="your api token"
+  platform_url="https://api-dev.traceable.ai/graphql"
+  api_token="NWFkNjc5NWItNDA3Ny00MGQwLWE1MjMtZTc4ODYzMWRlMDZm"
+}
+
+resource "example_api_naming_rule" "example_naming_rule" {
+  name             = "meg-test-rule-all-env"
+  disabled         = false
+  regexes          = ["hello", "test", "123"]
+  values           = ["hello", "test", "number"]
+  service_names    = [""]
+  environment_names = [""]
 }
 
 
-resource "example_ip_range_rule" "my_ip_range" {
-    name     = "first_rule_2"
-    rule_action     = "RULE_ACTION_ALERT"
-    event_severity     = "LOW"
-    raw_ip_range_data = [
-        "1.1.1.1",
-        "3.3.3.3"
-    ]
-    environment=[]
-    expiration = "PT600S"
-    description="rule created from custom provider"
-}
-# resource "example_rate_limit_rule" "my_rate_limit" {
+# resource "example_ip_range_rule" "my_ip_range" {
 #     name     = "first_rule_2"
-#     rule_action     = "RULE_ACTION_ALLOW"
+#     rule_action     = "RULE_ACTION_ALERT"
 #     event_severity     = "LOW"
 #     raw_ip_range_data = [
 #         "1.1.1.1",
 #         "3.3.3.3"
 #     ]
-#     conditions=[
-#       {
-#         name     = "server1"
-#         size     = "t2.micro"
-#         location = "us-east-1"
-#       },
-#       {
-#         type     = "server1"
-#         size     = "t2.micro"
-#         location = "us-east-1"
-#       }
-#     ]
-#     # expiration = "PT600S"
+#     environment=[]
+#     expiration = "PT600S"
 #     description="rule created from custom provider"
 # }
+
