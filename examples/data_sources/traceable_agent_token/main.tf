8a7685a28668604c380fdb2ddaddbdd0691eae3b
Aditya Kumar <aditya.kumar@traceable.ai>
2024-06-09 21:23:23 +0530

    added notification providers

---
+++ examples/data_sources/traceable_agent_token/main.tf
@@ -0,0 +1,13 @@
+data "traceable_agent_token" "example" {
+  name = "tf-provider-token-testing"
+}
+
+output "agent_token" {
+  value = data.traceable_agent_token.example.token
+  sensitive = true
+}
+
+output "agent_token_creation_timestamp" {
+  value = data.traceable_agent_token.example.creation_timestamp
+
+}
