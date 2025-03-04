db5898663021ef4fb506bfc92dce4a7143cbe803
Aditya Kumar <adityakumar@Adityas-MacBook-Pro.local>
2025-03-04 12:54:20 +0530

    removed sq brackets

--- provider/rate_limiting/queries.go
+++ provider/rate_limiting/queries.go
@@ -331,7 +331,7 @@ const (
                                   leafCondition: {
                                       conditionType: USER_ID
                                       userIdCondition: {
-                                          userIdRegexes: [%s]
+                                          userIdRegexes: %s
                                           exclude: %t
                                       }
                                   }
@@ -340,7 +340,7 @@ const (
                                   leafCondition: {
                                       conditionType: USER_ID
                                       userIdCondition: {
-                                          userIds: [%s]
+                                          userIds: %s
                                           exclude: %t
                                       }
                                   }
@@ -350,7 +350,7 @@ const (
                                  leafCondition: {
                                      conditionType: REQUEST_SCANNER_TYPE
                                      requestScannerTypeCondition: {
-                                         scannerTypes: [%s]
+                                         scannerTypes: %s
                                          exclude: %t
                                      }
                                  }
@@ -360,7 +360,7 @@ const (
                                      leafCondition: {
                                          conditionType: IP_CONNECTION_TYPE
                                          ipConnectionTypeCondition: {
-                                             ipConnectionTypes: [%s]
+                                             ipConnectionTypes: %s
                                              exclude: %t
                                          }
                                      }
@@ -379,7 +379,7 @@ const (
 	IS_ASN_QUERY              = `{
                         leafCondition: {
                             conditionType: IP_ASN
-                            ipAsnCondition: { ipAsnRegexes: [%s], exclude: %t }
+                            ipAsnCondition: { ipAsnRegexes: %s, exclude: %t }
                         }
                     }`
 
@@ -387,7 +387,7 @@ const (
                          leafCondition: {
                              conditionType: IP_ORGANISATION
                              ipOrganisationCondition: {
-                                 ipOrganisationRegexes: [%s]
+                                 ipOrganisationRegexes: %s
                                  exclude: %t
                              }
                          }
@@ -406,7 +406,7 @@ const (
 	USER_AGENT_QUERY = `{
                             leafCondition: {
                                 conditionType: USER_AGENT
-                                userAgentCondition: { userAgentRegexes: [%s], exclude: %t }
+                                userAgentCondition: { userAgentRegexes: %s, exclude: %t }
                             }
                         }`
 
@@ -414,7 +414,7 @@ const (
                                      leafCondition: {
                                          conditionType: IP_ADDRESS
                                          ipAddressCondition: {
-                                             rawInputIpData: [%s]
+                                             rawInputIpData: %s
                                              exclude: %t
                                          }
                                      }
@@ -433,7 +433,7 @@ const (
 	EMAIL_DOMAIN_QUERY = `{
                            leafCondition: {
                                conditionType: EMAIL_DOMAIN
-                               emailDomainCondition: { emailRegexes: [%s], exclude: %t }
+                               emailDomainCondition: { emailRegexes: %s, exclude: %t }
                            }
                        }`
 
@@ -441,7 +441,7 @@ const (
                                  leafCondition: {
                                      conditionType: IP_LOCATION_TYPE
                                      ipLocationTypeCondition: {
-                                         ipLocationTypes: [%s]
+                                         ipLocationTypes: %s
                                          exclude: %t
                                      }
                                  }
