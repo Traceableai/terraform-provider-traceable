478cef9003ca8a6d6127300cf27948947e448791
adityaKumar9006 <aditya.kumar@harness.io>
2025-05-13 15:11:13 +0530

    fixed naming and disabled->ena

--- examples/resources/waap_configs/resource.tf
+++ examples/resources/waap_configs/resource.tf
@@ -3,29 +3,29 @@ resource "traceable_waap_config" "waap" {
   rule_configs=[
     { 
       rule_name="XSS"
-      disabled=false
+      enabled=true
       subrules=[
         {
-          sub_rule_name="IE XSS Filters - Attack (A href with a link)"
-          sub_rule_action="BLOCK"
+          name="IE XSS Filters - Attack (A href with a link)"
+          action="BLOCK"
         },
         {
-          sub_rule_name="IE XSS Filters - Attack (Applet Tag)"
-          sub_rule_action="MONITOR"
+          name="IE XSS Filters - Attack (Applet Tag)"
+          action="MONITOR"
         }
       ]
     },
     { 
       rule_name="HTTPProtocolAttack"
-      disabled=false
+      enabled=true
       subrules=[
         {
-          sub_rule_name="HTTP Header Injection Attack via headers"
-          sub_rule_action="MONITOR"
+          name="HTTP Header Injection Attack via headers"
+          action="MONITOR"
         },
         {
-          sub_rule_name="IIS 6.0 WebDAV buffer overflow: (CVE-2017-7269)"
-          sub_rule_action="DISABLE"
+          name="IIS 6.0 WebDAV buffer overflow: (CVE-2017-7269)"
+          action="DISABLE"
         }
       ]
     }
